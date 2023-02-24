e311ca2401b319cc5336e3b66b6b20f50ad0c9ef
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-02-24 11:13:08 -0500

    finished lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self,list_of_words):
+        match_word_list = []
+
+        for word in list_of_words:
+            if sorted([letter for letter in word])  == self.word_letters:
+                match_word_list.append(word)
+        return match_word_list 
+
+
+
+
